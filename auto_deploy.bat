8fccfa745ba964e7809349a6a6f0d34f8d2fc4e3
Jasurbek <jasurbek414@gmail.com>
2026-05-07 02:43:14 +0500

    fix: deploy skriptiga yangi fayllar qo'shildi

--- auto_deploy.bat
+++ auto_deploy.bat
@@ -10,8 +10,7 @@ git push origin master
 
 echo.
 echo 2. Serverga ulanib yangilanish yuklanmoqda...
-echo Iltimos, agar parol so'rasa, server parolini kiriting!
-ssh ubuntu@43.201.59.31 "rm -rf gilam_repo_temp && git clone https://github.com/Jasurbek414/gilam_umumiy.git gilam_repo_temp && cp gilam_repo_temp/deploy_full.sh ./deploy_full.sh && chmod +x deploy_full.sh && ./deploy_full.sh"
+ssh -i server_key.pem ubuntu@43.201.59.31 -o StrictHostKeyChecking=no "rm -rf gilam_repo_temp && git clone https://github.com/Jasurbek414/gilam_umumiy.git gilam_repo_temp && cp gilam_repo_temp/deploy_full.sh ./deploy_full.sh && chmod +x deploy_full.sh && ./deploy_full.sh"
 
 echo.
 echo ==========================================
