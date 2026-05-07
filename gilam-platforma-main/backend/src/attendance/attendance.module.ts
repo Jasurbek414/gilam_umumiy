d5277c17492be3098f859651bf3f312d7624e27e
Jasurbek <jasurbek414@gmail.com>
2026-05-07 07:52:44 +0500

    fix: deploy skriptiga yangi fayllar qo'shildi

--- gilam-platforma-main/backend/src/attendance/attendance.module.ts
+++ gilam-platforma-main/backend/src/attendance/attendance.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attendance } from './entities/attendance.entity';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
+import { AuthModule } from '../auth/auth.module';
+import { forwardRef } from '@nestjs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance])],
+  imports: [TypeOrmModule.forFeature([Attendance]), forwardRef(() => AuthModule)],
   controllers: [AttendanceController],
   providers: [AttendanceService],
   exports: [AttendanceService],
